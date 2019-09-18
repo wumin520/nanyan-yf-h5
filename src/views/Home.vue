83117a5ecc2a100e38f923a115b9717f20c241fc
wumin <wu.min@qianka.com>
2019-09-18 14:47:32 +0800

    初始化项目

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    home
+    <cube-slide :data="items"/>
   </div>
 </template>
 
@@ -10,6 +12,24 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  data () {
+    return {
+      items: [
+        {
+          url: 'http://www.didichuxing.com/',
+          image: '//webapp.didistatic.com/static/webapp/shield/cube-ui-examples-slide01.png'
+        },
+        {
+          url: 'http://www.didichuxing.com/',
+          image: '//webapp.didistatic.com/static/webapp/shield/cube-ui-examples-slide02.png'
+        },
+        {
+          url: 'http://www.didichuxing.com/',
+          image: '//webapp.didistatic.com/static/webapp/shield/cube-ui-examples-slide03.png'
+        }
+      ]
+    }
+  },
   name: 'home',
   components: {
     HelloWorld
